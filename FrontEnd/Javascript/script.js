ab9dcc81662e686ff40fce800e6f8edc967ff5fc
JULIEMANC <136808377+JULIEMANC@users.noreply.github.com>
2023-09-18 09:01:51 +0200

    Finit !

--- FrontEnd/Javascript/script.js
+++ FrontEnd/Javascript/script.js
@@ -1,14 +1,14 @@
 /*VARIABLE GLOBALE
 */
-/*recuperation de la du token de la session storage */
+/*Récupération de la du token de la session storage.*/
 let token = sessionStorage.token
-/*constante pour stocker la liste des travaux (un Set n'a pas de doublon)*/
+/*Constante pour stocker la liste des travaux (un Set n'a pas de doublon).*/
 const allWorks = new Set()
 const allCats = new Set()
-/*Pour pas avoir d'apparition non demandée */
+/*Pour pas avoir d'apparition non demandée.*/
 let modal = null
 let modalAddwork = null
-/*constante des elements html*/
+/*Constante des elements html.*/
 const gallery = document.querySelector(".gallery ")
 const btnfiltres = document.querySelector(".btnfiltres")
 const projectandMod = document.querySelector(".projectandMod")
@@ -29,22 +29,22 @@ let messageModal = document.getElementById("messageModal")
 /*
         INIT
 */
-/*fonction d'initialisation, elle permet de preparer les Set et d'appeler les fonction du code*/
+/*Fonction d'initialisation, elle permet de preparer les Set et d'appeler les fonction du code.*/
 async function init() {
-    /*on appel la fonction qu'on a crée (pour appeler une fonciton "async", il faut etre dans une fonction "async" et utilisé "await"*/
+    /*On appel la fonction qu'on a crée (pour appeler une fonciton "async", il faut etre dans une fonction "async" et utilisé "await."*/
     const works = await getDatabaseInfo("works")
 
-    /*pour chaque travaux de la liste, on les rajoute a notre constante allWorks*/
+    /*Pour chaque travaux de la liste, on les rajoute a notre constante allWorks.*/
     for (const work of works) {
         allWorks.add(work)
     }
     const cats = await getDatabaseInfo("categories")
 
-    /*pour chaque catégorie de la liste, on les rajoute a notre constante allCats*/
+    /*Pour chaque catégorie de la liste, on les rajoute a notre constante allCats.*/
     for (const cat of cats) {
         allCats.add(cat)
     }
-    /*on appel la fonction les plus importantes (quon regroupe)*/
+    /*On appel la fonction les plus importantes (quon regroupe).*/
     displayWorks()
     displayButton()
     isAdmin()
@@ -59,19 +59,18 @@ async function init() {
 }
 init()
 /*
-        API FONCTION
-        (add/get/delete, tous ce qui contient un fetch)
+        API FONCTION 
 */
-/*fonction pour contacter la base de donnée pour récupéré des info, le paraméttre type est du texte qui va se rajouter dans le lien */
+/*Fonction pour contacter la base de donnée pour récupéré des info, le paraméttre type est du texte qui va se rajouter dans le lien.*/
 async function getDatabaseInfo(type) {
-    //on enregistre dans response la réponse du serveur sur le lien donnée
+    /*On enregistre dans response la réponse du serveur sur le lien donnée.*/
     const response = await fetch(`http://localhost:5678/api/${type}`)
-    /*si il n'ya pas d'erreur*/
+    /*Si il n'ya pas d'erreur.*/
     if (response.ok) {
-        /*on renvoie la réponse sous format json*/
+        /*On renvoie la réponse sous format json.*/
         return response.json()
     } else {
-        /* sinon on affiche un log de la reponse pour trouver d'ou vient l'erreur*/
+        /*Sinon on affiche un log de la reponse pour trouver d'ou vient l'erreur.*/
         console.log("echec du fetch", response)
     }
 }
@@ -79,10 +78,10 @@ async function getDatabaseInfo(type) {
         GALLERY FONCTION
         (toutes les fonctions qui concernent la gallerie principale (hors modal) et les filtres)
 */
-/*fonction qui permettra d'afficher les images(travaux=works) sur la page. */
+/*Fonction qui permettra d'afficher les images(travaux=works) sur la page.*/
 function displayWorks(filtre = "0") {
 
-    // supprime la gallerie du HTML grace aux "" vide en disant que la galerie de l'html est égale à un vide .
+    //Supprime la gallerie du HTML grace aux "" vide en disant que la galerie de l'html est égale à un vide .
     gallery.innerHTML = "";
 
     /*Triage des filtres en fonction du categoryID ( si filtres est different à 0 alirs on prends categoryId qui est = aux autres filtres.*/
@@ -91,7 +90,7 @@ function displayWorks(filtre = "0") {
         console.log(selectedWorks)
         selectedWorks = [...selectedWorks].filter((work) => work.categoryId == filtre)
     }
-    /*Création de la gallerie via JVS => html */
+    /*Création de la gallerie via JVS => html.*/
     const fragment = document.createDocumentFragment()
     for (const work of selectedWorks) {
         const figure = document.createElement("figure")
@@ -102,19 +101,19 @@ function displayWorks(filtre = "0") {
     }
     gallery.appendChild(fragment)
 }
-/*fonction "play" des filtres*/
+/*Fonction "play" des filtres.*/
 function displayButton() {
     const fragment = document.createDocumentFragment()
-    /*Filtre Tous */
+    /*Filtre Tous.*/
     const buttonTous = document.createElement("button")
     buttonTous.dataset.id = "0"
     buttonTous.textContent = "Tous"
     buttonTous.className = "filtres"
-    buttonTous.classList.add("active")//ajouter la classe active au btn Tous (+css)
+    buttonTous.classList.add("active")
 
     fragment.appendChild(buttonTous)
 
-    /* Création des autres filtres avec une boucle for*/
+    /*Création des autres filtres avec une boucle for.*/
     for (const category of allCats) {
         const buttonOthers = document.createElement("button")
         buttonOthers.textContent = category.name
@@ -123,15 +122,11 @@ function displayButton() {
         fragment.appendChild(buttonOthers)
     }
     btnfiltres.appendChild(fragment)
-    /*Ajout de l'écoute des filtres*/
+    /*Ajout de l'écoute des filtres.*/
     addFilterListener()
-
 }
 /*
-        ADMIN FUNCTION
-        (function qui gere l'affichage admin)  Mode edition si token et ID valides
- affichage de la banniere mode édition et apparition des boutons LogOut et modifier
-disparition de Login et des filtres
+        ADMIN FUNCTION  (function qui gere l'affichage admin)  Mode edition si token et ID valides
 */
 function isAdmin() {
     let userId = sessionStorage.getItem("userId")
@@ -140,21 +135,18 @@ function isAdmin() {
     if (userId != null & token != null) {
         let banner = document.querySelector(".overlay")
         banner.style.display = null
-
-        /* button login disparai*/
+        /*Bouton login disparait.*/
         let connexion = document.getElementById("connexion")
         connexion.style.display = "none"
         connexion.style.padding = "0px"
-
-        /* bouton logout apparait*/
+        /*Bouton logout apparait.*/
         let deconnexion = document.getElementById("deconnexion")
         deconnexion.style.display = null
         deconnexion.addEventListener("click", function () {
             sessionStorage.clear()
             window.location.reload()
         })
-
-        /*apparitions des boutons*/
+        /*Apparitions des boutons.*/
         let upChange = document.querySelector(".upChange")
         let buttonModal = document.getElementById("buttonModal")
         let logoMod = document.querySelector(".logoMod")
@@ -162,30 +154,28 @@ function isAdmin() {
         buttonModal.style.display = null
         logoMod.style.display = null
 
-        /*disparition des filtres */
+        /*disparition des filtres.*/
         let filtre = document.querySelector(".btnfiltres")
         filtre.style.display = "none"
     }
 }
 /*
-        MODAL FONCTION
-        fonciton qui gere l'affichage des modales
+        MODAL FONCTION,fonction qui gère l'affichage de la modal1.
 */
 function setupModal() {
     const openModal = function (e) {
-
-        /*Pour fermer boite modal avec button */
+        /*Pour fermer boite modal avec button.*/
         const closeModal = function (e) {
             if (modal == null) return
             e.preventDefault()
             modal.style.display = "none"
         }
-        /* Pour pas que la boite se ferme si on appuie dessus hors le button */
+        /* Pour que la modal se ferme quand on clique en dehors.*/
         const stopPropagation = function (e) {
             e.stopPropagation()
         }
         e.preventDefault()
-        /* recupere href:modifier qui ouvre la modal */
+        /*Récupère href:modifier qui ouvre la modal.*/
         const target = document.querySelector(e.target.getAttribute('href'))
         target.style.display = null
 
@@ -198,31 +188,30 @@ function setupModal() {
         modal.querySelector('.js-modal-close').addEventListener('click', closeModal)
         modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
     }
-    // sinon créer une boucle infini, car la on récupére les modal et on leur donne le open en listener, donc faut pas etre dans le open
+    /*Sinon créer une boucle infini, car la on récupére les modal et on leur donne le open en listener, donc faut pas etre dans le open.*/
     const modals = document.querySelectorAll(".js-modal") //
     modals.forEach(a => {
         a.addEventListener("click", openModal)
     })
 }
-/* cration de la gallery de la modal 1 */
-/* MENTOR : si je deplace dans API fonction en haut ca fait tout bugger */
+/* Création de la gallery de la modal1.*/
 async function displayModal() {
     galleryModal.innerHTML = ""
     for (const work of allWorks) {
 
-        let figure = document.createElement("figure") // nous avons effacer et la nous recreons les img and comment
+        let figure = document.createElement("figure") //Nous avons effacer et la nous recreons les img and comment.
         let img = document.createElement("img")
         let figcaption = document.createElement("figcaption")
-        figcaption.textContent = "éditer"// commentaire sous img editer
+        figcaption.textContent = "éditer"//Commentaire sous img editer.
         img.src = work.imageUrl
         img.alt = work.title
 
-        /* deletIcon*/
+        /* deletIcon.*/
         let deleteIcon = document.createElement("i")
-        deleteIcon.classList.add("fa-solid", "fa-trash-can", "delete-icon") // src icone
+        deleteIcon.classList.add("fa-solid", "fa-trash-can", "delete-icon") // src icone.
         deleteIcon.dataset.id = work.id
 
-        /* ajout des parents aux nvx éléments*/
+        /*Ajout des parents aux nvx éléments.*/
         figcaption.appendChild(deleteIcon)
         figure.appendChild(img)
         figure.appendChild(figcaption)
@@ -233,7 +222,7 @@ async function displayModal() {
     setDeleteListener()
 }
 
-/* delete icone poubelle dans le back */
+/*Delete icone poubelle dans le back.*/
 function setDeleteListener() {
     const deleteIcon = document.querySelectorAll(".delete-icon")
     for (const icon of deleteIcon) {
@@ -244,7 +233,7 @@ function setDeleteListener() {
                 method: 'DELETE',
                 headers: {
                     "accept": "application/json",
-                    "Authorization": `Bearer ${token}` // declarer en haut de page
+                    "Authorization": `Bearer ${token}` //Déclarer en haut de page.
                 }
             })
             if (response.ok) {
@@ -264,19 +253,19 @@ function setDeleteListener() {
         })
     }
 }
-// // // // MODAL ADDWORK//
+ /* MODAL ADDWORK */
 function setUpModalAddWork() {
-    /*récuperation du bouton  modal 1 à modal addwork*/
+    /*Récuperation du bouton  modal 1 à modal addwork.*/
     const buttonAddPhotos = document.querySelector(".buttonAddPhotos")
-    /*ajout d'un evenement click au btn ajouter photo modal 1 qui ouvre la 2*/
+    /*Ajout d'un evenement click au btn ajouter photo modal 1 qui ouvre la 2.*/
     buttonAddPhotos.addEventListener("click", () => {
-        modalAddwork.style.display = "grid" //apparaitre modaladdWork
-        modal1.style.display = "none"//disparaitre modal 1 au click de la seconde
+        modalAddwork.style.display = "grid" //Apparaitre modaladdWork.
+        modal1.style.display = "none"//Disparaitre modal 1 au click de la seconde.
     })
-    /* Récupérer la modal-addwork*/
+    /* Récupérer la modal-addwork.*/
     const modalAddwork = document.querySelector(".modal-addwork")
 
-    /* Récuperer fleche retour.*/
+    /*Récuperer fleche retour.*/
     const backArrow = document.querySelector(".fa-solid.fa-arrow-left")
 
     /*Ajout element click a la fleche retour.*/
@@ -285,13 +274,14 @@ function setUpModalAddWork() {
         modalAddwork.style.display = "none" //Masquer modalAddwork.
         modal1.style.display = "grid"
     })
-    /*Récuperation croix close modalAddwork.*/
-    // const closeModal = function (e) {
-    // const stopPropagation = function (e) {
-    //     e.stopPropagation()
-    // }
-    // e.preventDefault()
-    // modalAddwork.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
+    const closeModal = document.getElementById("modal-addwork");
+    closeModal.addEventListener("click", function (e) {
+        if (e.target === closeModal) {
+            modalAddwork.style.display = "none";
+        }
+        e.stopPropagation();
+    })
+    /*Récupération de l'icone croix pour fermer modal2.*/
     const closeIcon = document.querySelector("#close2")
     /*Ajout dun listener sur le bouton pr fermer la modal.*/
     closeIcon.addEventListener("click", function (e) {
@@ -315,10 +305,10 @@ async function categorySelect() {
         })
         .catch(error => console.error(error))
 }
-/*Ecoute du click sur le bouton ajout photo */
+/*Ecoute du click sur le bouton ajout photo.*/
 function previewSelectedImage() {
     const file = imageInput.files[0]
-    // Test image (taille <4mo) else alert()
+    // Test image (taille <4mo) .
     if (file) {
         const reader = new FileReader()
         reader.readAsDataURL(file)
@@ -338,12 +328,11 @@ function previewSelectedImage() {
     }
 }
 function checkInput() {
-    /*récuperation des elements de la modalAddWork*/
+    /*Récuperation des elements de la modalAddWork.*/
     if (titleInput.value !== "" && categoryInput.value !== "" && imageInput.value !== "") {
         buttonValidate.style.background = "#1D6154"
     }
 }
-
 function resetForm() {
     if (imgSelect !== null) {
         formAddWork.reset()
@@ -353,31 +342,28 @@ function resetForm() {
     document.querySelector(".before-img-select").style.display = "block"
 }
 
-/*Ajouter un événement pour gérer les messages d'erreur*/
+/*Ajouter un événement pour gérer les messages d'erreur.*/
 formAddWork.addEventListener("submit", async function (event) {
-    event.preventDefault() // Empêcher le rechargement de la page
+    event.preventDefault() // Empêcher le rechargement de la page.
     try {
-        /* Vérifier si l'image est charger*/
+        /* Vérifier si l'image est charger.*/
         if (imageInput.value === "") {
             messageModal.innerHTML = "Veuillez mettre un image."
             messageModal.style.color = "red"
             return
         }
-
-        /*Vérifier si le champ titre est rempli*/
+        /*Vérifier si le champ titre est rempli.*/
         if (titleInput.value === "") {
             messageModal.innerHTML = "Veuillez mettre un titre valide."
             messageModal.style.color = "red"
             return
         }
-
-        /* Vérifier si la catégorie est selectionner*/
+        /* Vérifier si la catégorie est selectionner.*/
         if (categoryInput.value === "") {
             messageModal.innerHTML = "Veuillez sélectionner une catégorie."
             messageModal.style.color = "red"
             return
         }
-
         /*Récupérer le token depuis la sessionStorage en haut et créer les données à envoyer à l'API*/
         const formData = new FormData()
         formData.append("image", imageInput.files[0])
@@ -385,13 +371,13 @@ formAddWork.addEventListener("submit", async function (event) {
         formData.append("category", categoryInput.value)
 
         const newWork = await addNewWork(formData)
-        /*creer les elements à ajouter ds le DOM */
+        /*Créer les elements à ajouter ds le DOM. */
         allWorks.add(newWork)
         displayWorks()
         displayModal()
-        /*Réinitialiser le formulaire*/
+        /*Réinitialiser le formulaire.*/
         resetForm()
-        /*Ajouter un message de succès (compris dans la fonction plus haut )*/
+        /*Ajouter un message de succès (compris dans la fonction plus haut ).*/
         messageModal.innerHTML = "L'image a été envoyée avec succès!", "success";
         messageModal.style.color = "#000"
     } catch (error) {
@@ -420,16 +406,16 @@ function addFilterListener() {
         filter.addEventListener("click", (e) => {
             const button = e.target
             const id = button.dataset.id
-            //Si le bouton est actif on remove les autres filtres
+            //Si le bouton est actif on remove les autres filtres.
             document.querySelector(".active").classList.remove("active")
             button.classList.add("active")
             displayWorks(id)
         })
     }
 }
-/*Pour ajouter dans la modaladdwork l'image sélectionner */
+/*Pour ajouter dans la modaladdwork l'image sélectionner. */
 imageInput.addEventListener('change', previewSelectedImage)
-/*Ajouter des écouteurs d'événements pour les champs de saisie*/
+/*Ajouter des écouteurs d'événements pour les champs de saisie.*/
 titleInput.addEventListener("change", checkInput)
 categoryInput.addEventListener("change", checkInput)
 imageInput.addEventListener("change", checkInput)
